b58917be5d471c9c399d02b06ef7829a9c15d20f
Sarai <79231677+SJFortin92@users.noreply.github.com>
2021-03-06 16:17:10 -0500

    Started Fullcalendar integration

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,16 @@ namespace BabysFirstCalendar.Controllers
 
             return View();
         }
+
+        //Function to retrieve memories from the database
+        public JsonResult GetMemories()
+        {
+            using (NewbornCalendarModelEntities dc = new NewbornCalendarModelEntities())
+            {
+                var memories = dc.Notes.ToList();
+                return new JsonResult { Data = memories, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
     }
 
 }
